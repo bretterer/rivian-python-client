a54bc5ab231650db6709ea873940a74f0c45b811
Brian Retterer <bretterer@gmail.com>
2022-05-26 17:39:56 -0400

    WIP

--- src/rivian/rivian.py
+++ src/rivian/rivian.py
@@ -1,41 +1,386 @@
 """Asynchronous Python client for the Rivian API."""
 from __future__ import annotations
+
+from typing import Any
+
 import asyncio
 import json
+import socket
 
-from typing import Any, Mapping
-
-from .vehicleInfo import RivianVehicleInfo
+import aiohttp
+import async_timeout
+from yarl import URL
 
 class Rivian:
-    """Main class for handling connections with the Rivian API."""
+    """Main class for the Rivian API Client"""
+
     def __init__(
         self,
-        username: str | None = None,
-        password: str | None = None,
-        client_id: str | None = None,
-        client_secret: str | None = None,
+        client_id: str,
+        client_secret: str,
+        request_timeout: int = 10,
+        session: aiohttp.client.ClientSession | None = None,
     ) -> None:
-        """Initialize connection with the Rivian API.
-        Class constructor for setting up an Rivian object to
-        communicate with the Rivian API instance.
-        Args:
-            username: Username for your Rivian Account.
-            password: Password for your Rivian Account.
-            client_id: The Client ID for the Rivian API.
-            client_secret: The Client secret for the Rivian API.
-        """
-        self._auth_url = "https://auth.rivianservices.com"
-        self._base_url = "https://rivian.com"
+        self._session = session
+        self._close_session = False
+        self._session_token = ""
+        self._access_token = ""
+        self._refresh_token = ""
 
-        self.vehicle_info = RivianVehicleInfo(self)
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.request_timeout = request_timeout
 
-    async def request(
+        self._otp_needed = False
+
+
+    async def authenticate(
         self,
-        uri: str,
-        method: str = "GET",
-        data: Any | None = None,
-        json_data: dict | None = None,
-        params: Mapping[str, str] | None = None,
+        username: str,
+        password: str,
     ) -> dict[str, Any]:
-        """Handle a request to the Rivian API."""
+        """Authenticate against the Rivian API with Username and Password"""
+        url = "https://auth.rivianservices.com/auth/api/v1/token/auth"
+
+        headers = {
+            "User-Agent": "RivianApp/707 CFNetwork/1237 Darwin/20.4.0",
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+        json_data = {
+            "grant_type": "password",
+            "username": username,
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "pwd": password
+        }
+
+        if self._session is None:
+            self._session = aiohttp.ClientSession()
+            self._close_session = True
+
+        try:
+            async with async_timeout.timeout(self.request_timeout):
+                response = await self._session.request(
+                    "POST",
+                    url,
+                    json=json_data,
+                    headers=headers,
+                )
+        except asyncio.TimeoutError as exception:
+            raise Exception(
+                "Timeout occurred while connecting to Rivian API."
+            ) from exception
+        except (aiohttp.ClientError, socket.gaierror) as exception:
+            raise Exception(
+                "Error occurred while communicating with Rivian."
+            ) from exception
+
+        content_type = response.headers.get("Content-Type", "")
+
+
+
+        if response.status == 401:
+            self._otp_needed = True
+            response_json = await response.json()
+            self._session_token = response_json["session_token"]
+            return response
+
+        if response.status // 100 in [4, 5]:
+            contents = await response.read()
+            response.close()
+
+            if content_type == "application/json":
+                raise Exception(
+                    response.status, json.loads(contents.decode("utf8"))
+                )
+            raise Exception(
+                response.status, {"message": contents.decode("utf8")}
+            )
+
+        if "application/json" in content_type:
+            response_json = await response.json()
+            self._access_token = response_json["access_token"]
+            self._refresh_token = response_json["refresh_token"]
+            return response
+
+        text = await response.text()
+        return {"message": text}
+
+    async def validate_otp(
+        self,
+        username: str,
+        otp: str,
+    ) -> dict[str, Any]:
+        """Validate the OTP"""
+        url = "https://auth.rivianservices.com/auth/api/v1/token/auth"
+
+        headers = {
+            "User-Agent": "RivianApp/707 CFNetwork/1237 Darwin/20.4.0",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self._session_token
+        }
+
+        json_data = {
+            "grant_type": "password",
+            "username": username,
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "otp_token": otp,
+        }
+
+        if self._session is None:
+            self._session = aiohttp.ClientSession()
+            self._close_session = True
+
+        try:
+            async with async_timeout.timeout(self.request_timeout):
+                response = await self._session.request(
+                    "POST",
+                    url,
+                    json=json_data,
+                    headers=headers,
+                )
+        except asyncio.TimeoutError as exception:
+            raise Exception(
+                "Timeout occurred while connecting to Rivian API."
+            ) from exception
+        except (aiohttp.ClientError, socket.gaierror) as exception:
+            raise Exception(
+                "Error occurred while communicating with Rivian."
+            ) from exception
+
+        content_type = response.headers.get("Content-Type", "")
+
+        if response.status // 100 in [4, 5]:
+            contents = await response.read()
+            response.close()
+
+            if content_type == "application/json":
+                raise Exception(
+                    response.status, json.loads(contents.decode("utf8"))
+                )
+            raise Exception(
+                response.status, {"message": contents.decode("utf8")}
+            )
+
+        if "application/json" in content_type:
+            response_json = await response.json()
+            self._access_token = response_json["access_token"]
+            self._refresh_token = response_json["refresh_token"]
+            return response
+
+        text = await response.text()
+        return {"message": text}
+
+    async def refresh_access_token(
+        self,
+        refresh_token: str,
+        client_id: str,
+        client_secret: str,
+    ) -> dict[str, Any]:
+        """Validate the OTP"""
+        url = "https://auth.rivianservices.com/auth/api/v1/token/auth"
+
+        headers = {
+            "User-Agent": "RivianApp/707 CFNetwork/1237 Darwin/20.4.0",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+
+        json_data = {
+            "token": refresh_token,
+            "client_id": client_id,
+            "client_secret": client_secret,
+        }
+
+        if self._session is None:
+            self._session = aiohttp.ClientSession()
+            self._close_session = True
+
+        try:
+            async with async_timeout.timeout(self.request_timeout):
+                response = await self._session.request(
+                    "POST",
+                    url,
+                    json=json_data,
+                    headers=headers,
+                )
+        except asyncio.TimeoutError as exception:
+            raise Exception(
+                "Timeout occurred while connecting to Rivian API."
+            ) from exception
+        except (aiohttp.ClientError, socket.gaierror) as exception:
+            raise Exception(
+                "Error occurred while communicating with Rivian."
+            ) from exception
+
+        content_type = response.headers.get("Content-Type", "")
+
+        if response.status // 100 in [4, 5]:
+            contents = await response.read()
+            response.close()
+
+            if content_type == "application/json":
+                raise Exception(
+                    response.status, json.loads(contents.decode("utf8"))
+                )
+            raise Exception(
+                response.status, {"message": contents.decode("utf8")}
+            )
+
+        if "application/json" in content_type:
+            response_json = await response.json()
+            self._access_token = response_json["access_token"]
+            return response
+
+        text = await response.text()
+        return {"message": text}
+
+    async def get_vehicle_info(
+        self,
+        vin: str,
+    ) -> dict[str, Any]:
+        """get the vehicle info"""
+        url = "https://cesium.rivianservices.com/v2/vehicle/latest"
+
+        headers = {
+            "User-Agent": "RivianApp/707 CFNetwork/1237 Darwin/20.4.0",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self._access_token
+        }
+
+        json_data = {
+            "car":vin,
+            "properties": [
+                "body/closures/door_FL_state",
+                "body/closures/door_FL_locked_state",
+                "body/closures/door_FR_state",
+                "body/closures/door_FR_locked_state",
+                "body/closures/door_RL_state",
+                "body/closures/door_RL_locked_state",
+                "body/closures/door_RR_state",
+                "body/closures/door_RR_locked_state",
+                "body/closures/frunk_state",
+                "body/closures/frunk_locked_state",
+                "body/closures/tailgate_state",
+                "body/closures/tailgate_locked_state",
+                "body/closures/liftgate_state",
+                "body/closures/liftgate_locked_state",
+                "body/closures/tonneau_state",
+                "body/closures/tonneau_locked_state",
+                "body/closures/sidebin_L_state",
+                "body/closures/sidebin_L_locked_state",
+                "body/closures/sidebin_R_state",
+                "body/closures/sidebin_R_locked_state",
+                "body/closures/front_left_window_state",
+                "body/closures/front_right_window_state",
+                "body/closures/rear_left_window_state",
+                "body/closures/rear_right_window_state",
+                "body/closures/gear_guard_locked_state",
+                "$gnss",
+                "energy_storage/charger/adjusted_soc",
+                "energy_storage/charger/vehicle_charger_state",
+                "energy_storage/charger/EMS_charger_remainingtime_min_1",
+                "energy_storage/vehicle_energy/vehicle_range",
+                "energy_storage/icd_cid_notifications/range_threshold",
+                "dynamics/propulsion_status/PRNDL",
+                "thermal/tmm_status/cabin_precondition_state",
+                "thermal/hvac_cabin_control/cabin_temperature",
+                "body/alarm/sound_alarm",
+                "telematics/ota_status/current_version_year",
+                "telematics/ota_status/current_version_week",
+                "telematics/ota_status/current_version_number",
+                "telematics/ota_status/available_version_year",
+                "telematics/ota_status/available_version_week",
+                "telematics/ota_status/available_version_number",
+                "telematics/ota_status/status",
+                "telematics/ota_status/install_duration",
+                "telematics/ota_status/install_progress",
+                "telematics/ota_status/pending_reason_active_mode",
+                "core/ota_status/cgm_ota_install_fast_charging",
+                "core/ota_status/cgm_ota_install_hv_batt_low",
+                "telematics/ota_status/pending_reason_lv_batt",
+                "core/ota_status/cgm_ota_install_not_parked",
+                "telematics/ota_status/pending_reason_other",
+                "telematics/ota_status/status_current",
+                "dynamics/odometer/value",
+                "dynamics/tires/tire_FL_pressure_status",
+                "dynamics/tires/tire_FR_pressure_status",
+                "dynamics/tires/tire_RL_pressure_status",
+                "dynamics/tires/tire_RR_pressure_status",
+                "dynamics/tires/tire_FL_pressure_status_valid",
+                "dynamics/tires/tire_FR_pressure_status_valid",
+                "dynamics/tires/tire_RL_pressure_status_valid",
+                "dynamics/tires/tire_RR_pressure_status_valid",
+                "dynamics/powertrain_status/brake_fluid_level_low",
+                "body/wipers/fluid_state",
+                "core/power_modes/power_state"
+            ]
+        }
+
+        if self._session is None:
+            self._session = aiohttp.ClientSession()
+            self._close_session = True
+
+        try:
+            async with async_timeout.timeout(self.request_timeout):
+                response = await self._session.request(
+                    "POST",
+                    url,
+                    json=json_data,
+                    headers=headers,
+                )
+        except asyncio.TimeoutError as exception:
+            raise Exception(
+                "Timeout occurred while connecting to Rivian API."
+            ) from exception
+        except (aiohttp.ClientError, socket.gaierror) as exception:
+            raise Exception(
+                "Error occurred while communicating with Rivian."
+            ) from exception
+
+        content_type = response.headers.get("Content-Type", "")
+
+        if response.status // 100 in [4, 5]:
+            contents = await response.read()
+            response.close()
+
+            if content_type == "application/json":
+                raise Exception(
+                    response.status, json.loads(contents.decode("utf8"))
+                )
+            raise Exception(
+                response.status, {"message": contents.decode("utf8")}
+            )
+
+        if "application/json" in content_type:
+            response_json = await response.json()
+            self._access_token = response_json["access_token"]
+            self._refresh_token = response_json["refresh_token"]
+            return response_json
+
+        text = await response.text()
+        return {"message": text}
+
+    async def close(self) -> None:
+        """Close open client session."""
+        if self._session and self._close_session:
+            await self._session.close()
+
+    async def __aenter__(self) -> Rivian:
+        """Async enter.
+        Returns:
+            The Rivian object.
+        """
+        return self
+
+    async def __aexit__(self, *_exc_info) -> None:
+        """Async exit.
+        Args:
+            _exc_info: Exec type.
+        """
+        await self.close()
---
+++ tests/rivian_test.py
@@ -0,0 +1,135 @@
+"""Tests for `rivian.rivian`."""
+import asyncio
+from unittest.mock import patch
+
+import aiohttp
+import pytest
+
+from rivian import Rivian
+
+@pytest.mark.asyncio
+async def test_authentication_request_with_otp(aresponses):
+    """Test JSON Response for an authentication request requiring OTP"""
+    aresponses.add(
+        "auth.rivianservices.com",
+        "/auth/api/v1/token/auth",
+        "POST",
+        aresponses.Response(
+            status=401,
+            headers={
+                "Content-Type": "application/json:"
+            },
+            text='''{
+                "api": "/auth/api/v1/token/auth",
+                "error": "otp_entry_required",
+                "error_description": "User is registered for MFA",
+                "result": -901,
+                "session_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMwOTQ3NzQsIm5iZiI6MTY1MzA5Mzg3MywiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJzZXNzaW9uX2lkIjoic2Vzc2lvbl9pZCJ9.bC84P5DBu517gjARQhR9FRIsI_Hv1mWYBy_OD52QVgI"
+            }'''
+        ),
+    )
+    async with aiohttp.ClientSession() as session:
+        rivian = Rivian("abcd1234", "wxyz9876")
+        response = await rivian.authenticate("username", "password")
+        response_json = await response.json()
+        assert response.status == 401
+        assert response_json["error_description"] == "User is registered for MFA"
+        assert response_json["session_token"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMwOTQ3NzQsIm5iZiI6MTY1MzA5Mzg3MywiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJzZXNzaW9uX2lkIjoic2Vzc2lvbl9pZCJ9.bC84P5DBu517gjARQhR9FRIsI_Hv1mWYBy_OD52QVgI"
+        await rivian.close()
+
+@pytest.mark.asyncio
+async def test_otp_validation(aresponses):
+    """Test JSON Response for an otp validation"""
+    aresponses.add(
+        "auth.rivianservices.com",
+        "/auth/api/v1/token/auth",
+        "POST",
+        aresponses.Response(
+            status=200,
+            headers={
+                "Content-Type": "application/json:"
+            },
+            text='''{
+                "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMxMDExMDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJ1c2VyX2lkIjoidXNlcl9pZCJ9.WsQIlNYTVxupCNJd3ohRbzReeoeqEdwjM9bYJkyTnu4",
+                "api": "/auth/api/v1/token/auth",
+                "expires_in": 7200,
+                "refresh_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Njg2NDU5MDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsInVzZXJfaWQiOiJ1c2VyX2lkIn0.Gu3SQt1la_SJwvU4hKccojygfTqFL1nWrxsEDv-ewks",
+                "result": 0,
+                "rights": {},
+                "token_type": "bearer"
+            }'''
+        ),
+    )
+    async with aiohttp.ClientSession() as session:
+        rivian = Rivian("abcd1234", "wxyz9876")
+        response = await rivian.validate_otp("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMwOTQ3NzQsIm5iZiI6MTY1MzA5Mzg3MywiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJzZXNzaW9uX2lkIjoic2Vzc2lvbl9pZCJ9.bC84P5DBu517gjARQhR9FRIsI_Hv1mWYBy_OD52QVgI", "123456")
+        assert response.status == 200
+        response_json = await response.json()
+        assert response_json["expires_in"] == 7200
+        assert response_json["access_token"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMxMDExMDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJ1c2VyX2lkIjoidXNlcl9pZCJ9.WsQIlNYTVxupCNJd3ohRbzReeoeqEdwjM9bYJkyTnu4"
+        assert response_json["refresh_token"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Njg2NDU5MDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsInVzZXJfaWQiOiJ1c2VyX2lkIn0.Gu3SQt1la_SJwvU4hKccojygfTqFL1nWrxsEDv-ewks"
+        await rivian.close()
+
+@pytest.mark.asyncio
+async def test_authentication_request_without_otp(aresponses):
+    """Test JSON Response for an authentication request without OTP"""
+    aresponses.add(
+        "auth.rivianservices.com",
+        "/auth/api/v1/token/auth",
+        "POST",
+        aresponses.Response(
+            status=200,
+            headers={
+                "Content-Type": "application/json:"
+            },
+            text='''{
+                "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMxMDExMDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJ1c2VyX2lkIjoidXNlcl9pZCJ9.WsQIlNYTVxupCNJd3ohRbzReeoeqEdwjM9bYJkyTnu4",
+                "api": "/auth/api/v1/token/auth",
+                "expires_in": 7200,
+                "refresh_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Njg2NDU5MDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsInVzZXJfaWQiOiJ1c2VyX2lkIn0.Gu3SQt1la_SJwvU4hKccojygfTqFL1nWrxsEDv-ewks",
+                "result": 0,
+                "rights": {},
+                "token_type": "bearer"
+            }'''
+        ),
+    )
+    async with aiohttp.ClientSession() as session:
+        rivian = Rivian("abcd1234", "wxyz9876")
+        response = await rivian.authenticate("username", "password")
+        assert response.status == 200
+        response_json = await response.json()
+        assert response_json["expires_in"] == 7200
+        assert response_json["access_token"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMxMDExMDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJ1c2VyX2lkIjoidXNlcl9pZCJ9.WsQIlNYTVxupCNJd3ohRbzReeoeqEdwjM9bYJkyTnu4"
+        assert response_json["refresh_token"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Njg2NDU5MDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsInVzZXJfaWQiOiJ1c2VyX2lkIn0.Gu3SQt1la_SJwvU4hKccojygfTqFL1nWrxsEDv-ewks"
+        await rivian.close()
+
+@pytest.mark.asyncio
+async def test_refresh_access_token(aresponses):
+    """Test JSON Response for refreshing access_token"""
+    aresponses.add(
+        "auth.rivianservices.com",
+        "/auth/api/v1/token/auth",
+        "POST",
+        aresponses.Response(
+            status=200,
+            headers={
+                "Content-Type": "application/json:"
+            },
+            text='''{
+                "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMxMDExMDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJ1c2VyX2lkIjoidXNlcl9pZCJ9.WsQIlNYTVxupCNJd3ohRbzReeoeqEdwjM9bYJkyTnu4",
+                "api": "/auth/api/v1/token/refresh",
+                "app_id": "RIVIAN_MOBILE",
+                "expires_in": 7200,
+                "result": 0,
+                "token_type": "bearer"
+            }'''
+        ),
+    )
+    async with aiohttp.ClientSession() as session:
+        rivian = Rivian("abcd1234", "wxyz9876")
+        response = await rivian.refresh_access_token("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMwOTQ3NzQsIm5iZiI6MTY1MzA5Mzg3MywiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJzZXNzaW9uX2lkIjoic2Vzc2lvbl9pZCJ9.bC84P5DBu517gjARQhR9FRIsI_Hv1mWYBy_OD52QVgI", "abcd1234", "wxyz9876")
+        assert response.status == 200
+        response_json = await response.json()
+        assert response_json["expires_in"] == 7200
+        assert response_json["access_token"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTMxMDExMDIsIm5iZiI6MTY1MzA5MzkwMSwiaXNzIjoicml2aWFuLmNvbSIsImF1ZCI6WyJodHRwczovL2Nsb3VkLnJpdmlhbnNlcnZpY2VzLmNvbS8iLCJodHRwczovL2F1dGgucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vKi1zZXJ2aWNlLWNocmctKi5wcm9kIiwiaHR0cHM6Ly9kdC1ncWwtYWNjb3VudC1tYW5hZ2VyLnByb2QuKi5kYy5nb3Jpdi5jbyIsImh0dHBzOi8vdHJpcC1wcm9kLnJpdmlhbnNlcnZpY2VzLmNvbSIsImh0dHBzOi8vaWQucml2aWFuc2VydmljZXMuY29tLyIsImh0dHBzOi8vZGF0YS5yaXZpYW5zZXJ2aWNlcy5jb20vIiwiaHR0cHM6Ly9hcHBzLmdvcml2LmNvL2FwaS92cy9ncWwtZ2F0ZXdheSJdLCJzY29wZSI6IioiLCJ1c2VyX2lkIjoidXNlcl9pZCJ9.WsQIlNYTVxupCNJd3ohRbzReeoeqEdwjM9bYJkyTnu4"
+        await rivian.close()
