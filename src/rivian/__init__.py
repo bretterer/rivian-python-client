1023bd34dca0988408a8f09732ccfb597c2797a3
Greg Rhoades <info@grhoades.net>
2026-08-05 18:49:12 -0400

    feat(parallax): add Parallax WebSocket subscription and protobuf decoders

--- src/rivian/__init__.py
+++ src/rivian/__init__.py
@@ -1,6 +1,47 @@
 """Asynchronous Python client for the Rivian API."""
 
 from .const import VehicleCommand
+from .parallax import (
+    CHARGING_RVMS,
+    PARALLAX_RVMS,
+    RVM_DECODERS,
+    decode_battery_state,
+    decode_cabin_temperatures,
+    decode_charge_session_breakdown,
+    decode_charging_graph_global,
+    decode_charging_session_status,
+    decode_closures,
+    decode_defrost,
+    decode_gnss,
+    decode_locks,
+    decode_odometer,
+    decode_parallax_message,
+    decode_power_state,
+    decode_preconditioning,
+    decode_time_estimation,
+    decode_tires,
+)
 from .rivian import Rivian
 
-__all__ = ["Rivian", "VehicleCommand"]
+__all__ = [
+    "CHARGING_RVMS",
+    "PARALLAX_RVMS",
+    "RVM_DECODERS",
+    "Rivian",
+    "VehicleCommand",
+    "decode_battery_state",
+    "decode_cabin_temperatures",
+    "decode_charge_session_breakdown",
+    "decode_charging_graph_global",
+    "decode_charging_session_status",
+    "decode_closures",
+    "decode_defrost",
+    "decode_gnss",
+    "decode_locks",
+    "decode_odometer",
+    "decode_parallax_message",
+    "decode_power_state",
+    "decode_preconditioning",
+    "decode_time_estimation",
+    "decode_tires",
+]
